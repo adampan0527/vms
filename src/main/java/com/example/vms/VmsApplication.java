b24b2ee1f6ddb95abe91a33f36a10931a71a083c
Adam <1241882726@qq.com>
2021-07-13 00:56:01 +0800

    2021.7.13

--- src/main/java/com/example/vms/VmsApplication.java
+++ src/main/java/com/example/vms/VmsApplication.java
@@ -1,6 +1,5 @@
 package com.example.vms;
 
-//import com.example.vms.config.CrosFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,15 +11,4 @@ public class VmsApplication {
         SpringApplication.run(VmsApplication.class, args);
     }
 
-    /**
-     * 配置跨域访问的过滤器
-     * @return
-     */
-//    @Bean
-//    public FilterRegistrationBean registerFilter(){
-//        FilterRegistrationBean bean = new FilterRegistrationBean();
-//        bean.addUrlPatterns("/*");
-//        bean.setFilter(new CrosFilter());
-//        return bean;
-//    }
 }
