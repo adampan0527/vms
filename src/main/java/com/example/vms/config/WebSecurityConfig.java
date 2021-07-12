05e8787b3c04840df58b523720e433a35f0fa0a4
Adam <1241882726@qq.com>
2021-07-13 01:48:00 +0800

    2021.7.13

--- src/main/java/com/example/vms/config/WebSecurityConfig.java
+++ src/main/java/com/example/vms/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers("/doc.html/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
